bffcb3bcace35d4abe71b7169f241391b9b8cbb0
MarkJGx <mark.jg@chemicalheads.com>
2024-09-19 20:36:47 +0300

    ywasm: as_value: pass through javascript with...
    rust closure to avoid unnecessary call overhead

--- ywasm/js/reflection.js
+++ ywasm/js/reflection.js
@@ -34,3 +34,31 @@ export function getTypeJs(target) {
         return 255;
     }
 }
+
+export function handleAsValue(target, functor) {
+    let resultEnum = -1;
+    let resultNumber = 0.0;
+
+    if (target === undefined) {
+        resultEnum = 0;
+    } else if (target === null) {
+        resultEnum = 1;
+    } else if (typeof target === 'number') {
+        resultEnum = 2;
+        resultNumber = target;
+    } else if (typeof target === 'boolean') {
+        resultEnum = 3;
+        resultNumber = target ? 1.0 : 0.0;
+    } else if (typeof target === 'bigint') {
+        resultEnum = 4;
+        resultNumber = Number(target);
+    } else if (typeof target === 'string') {
+        resultEnum = 5;
+    } else if (Array.isArray(target)) {
+        resultEnum = 6;
+    } else if (typeof(target) === 'object') {
+        resultEnum = 7;
+    }
+
+    functor(resultEnum, resultNumber);
+}
